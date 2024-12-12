d911c99704ea608d03782387a938b53570581330
livi90 <154985607+livi90@users.noreply.github.com>
2024-12-12 15:05:33 +0100

    Update  ANALITYCS TO layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import Navbar from "@/components/navbar";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
+import Script from 'next/script'; // Importa el componente Script
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,8 +20,27 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* Google tag (gtag.js) */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-SH56N4GCK6" // Reemplaza con tu ID de seguimiento
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-SH56N4GCK6'); // Reemplaza con tu ID de seguimiento
+            `,
+          }}
+        />
+      </head>
       <body
-        className={`${inter.className}  min-h-screen bg-black text-white antialiased`}
+        className={`${inter.className} min-h-screen bg-black text-white antialiased`}
       >
         <ThemeProvider
           attribute="class"
